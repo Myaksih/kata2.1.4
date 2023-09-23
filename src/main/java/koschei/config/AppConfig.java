efe254d9c992407031948f11070de3caba1e071c
Myaksih <nnkozhemyakin@yandex.ru>
2023-09-23 16:30:31 +0300

    done

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,12 @@
 package koschei.config;
 
+import koschei.models.Deth8;
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +18,10 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth8){
+        return new Needle7(deth8);
     }
 }
